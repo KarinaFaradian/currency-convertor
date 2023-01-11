853d33d22bb31461c00af39762613124647ebe9f
Карина Фарадян <faradiankarina@gmail.com>
2023-01-11 18:36:59 +0400

    build: added currency-convertor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
+import { Block } from './Block';
+import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState('RUB');
+  const [toCurrency, setToCurrency] = useState('USD')
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(1);
+
+  const quotesRef = useRef({});
+
+  let myHeaders = new Headers();
+  myHeaders.append("apikey", "xSNNRQsH3VIAzvRebTOrjd1cnIA6MeG3");
+
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+  
+  useEffect(() => {
+    fetch("https://api.apilayer.com/currency_data/live?source=JPY&currencies=EUR%2CRUB%2CUSD%2CGBP", requestOptions)
+    .then((response) => response.json())
+    .then((json) => {
+      quotesRef.current = json.quotes;
+      onChangeToPrice(1);
+    }).catch(err => {
+      console.warn(err);
+      alert('Error');
+    })
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / quotesRef.current[`JPY${fromCurrency}`];
+    const result = price * quotesRef.current[`JPY${toCurrency}`];
+    setFromPrice(value);
+    setToPrice(result.toFixed(3));
+  }
+
+  const onChangeToPrice = (value) => {
+    const result = (quotesRef.current[`JPY${fromCurrency}`] / quotesRef.current[`JPY${toCurrency}`]) * value;
+    setFromPrice(result.toFixed(3));
+    setToPrice(value);
+  }
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  },[fromCurrency])
+
+  useEffect(() => {
+    onChangeToPrice(toPrice);
+  },[toCurrency])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Block 
+        value={fromPrice} 
+        currency={fromCurrency} 
+        onChangeCurrency={setFromCurrency} 
+        onChangeValue={onChangeFromPrice} 
+      />
+      <Block 
+        value={toPrice} 
+        currency={toCurrency} 
+        onChangeCurrency={setToCurrency} 
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
